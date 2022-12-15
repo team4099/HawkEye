52c36a9a1d83291e265d1aa972e48a3a3691f556
Saraansh Wadkar <saraanshwadkar@gmail.com>
2022-12-14 20:41:09 -0500

    clean up hawkeye config

--- app/src/main/java/com/team4099/lib/photonvision/networking/HawkeyeConfig.kt
+++ app/src/main/java/com/team4099/lib/photonvision/networking/HawkeyeConfig.kt
@@ -3,36 +3,9 @@ package com.team4099.lib.photonvision.networking
 
 object HawkeyeConfig{
         var teamNumber: Int = -1337
-        //    var connectionType: NetworkMode = NetworkMode.DHCP
         var staticIp: String? = "10.0.2.2"
         var cameraName: String = "hawkeye"
         var runNTServer: Boolean = false
         var shouldManage: Boolean = false
         var tagSizeMeters: Double = 0.1524
-
-//    fun toHashMap(): HashMap<String, Any?> {
-//        val tmp = HashMap<String, Any?>()
-//        tmp["teamNumber"] = teamNumber
-//        tmp["supported"] = shouldManage
-////        tmp["connectionType"] = connectionType.ordinal()
-//        tmp["staticIp"] = staticIp
-//        tmp["hostname"] = hostname
-//        tmp["runNTServer"] = runNTServer
-//        return tmp
-//    }
-//
-//    object {
-//        fun fromHashMap(map: Map<String?, Any>): NetworkConfig {
-//            // teamNumber (int), supported (bool), connectionType (int),
-//            // staticIp (str), netmask (str), hostname (str)
-//            val ret = NetworkConfig(
-//                    map["teamNumber"].toString().toInt(),
-//                    map["staticIp"] as String?,
-//                    map["hostname"] as String?,
-//                    (map["runNTServer"] as Boolean?)!!,
-//                    ((map["supported"] as Boolean?)!!)
-//            )
-//            return ret
-//        }
-//    }
 }
